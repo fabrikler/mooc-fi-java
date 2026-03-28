b979335cd0ebf66ba9548064629ff99907bec8f9
fabrikler <hi@carlosmeyer.com>
2026-03-28 21:16:42 +0100

    square

--- part02/01-squared/Squared.java
+++ part02/01-squared/Squared.java
@@ -6,5 +6,11 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.valueOf(scanner.nextLine());
+
+        int square = num * num;
+
+        System.out.println(square);
+
     }
 }
