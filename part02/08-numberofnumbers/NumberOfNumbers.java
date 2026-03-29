af7d7b768ccd5e31ee16d773a52cb3a32cebc33c
fabrikler <hi@carlosmeyer.com>
2026-03-29 22:17:31 +0200

    number of numbers

--- part02/08-numberofnumbers/NumberOfNumbers.java
+++ part02/08-numberofnumbers/NumberOfNumbers.java
@@ -6,5 +6,23 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                break;
+            }
+
+            counter += 1;
+
+        }
+
+        System.out.println("Number of numbers: " + counter);
+
+
     }
 }
