10519b0cbdedaf57a91fde77bb830d60b110f4d9
fabrikler <hi@carlosmeyer.com>
2026-03-29 22:30:58 +0200

    num of neg num

--- part02/09-numberofnegativenumbers/NumberOfNegativeNumbers.java
+++ part02/09-numberofnegativenumbers/NumberOfNegativeNumbers.java
@@ -6,5 +6,22 @@ public class NumberOfNegativeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == 0) {
+                break;
+            }
+
+            if (num < 0) {
+                counter += 1;
+            }
+        }
+
+        System.out.println("Number of negative numbers: " + counter);
     }
 }
