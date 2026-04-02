660a86d6782cd857a8e7b363663c40afc5e0d669
fabrikler <hi@carlosmeyer.com>
2026-04-02 11:33:07 +0200

    more calcs

--- part02/18-sumofasequencethesequel/SumOfASequenceTheSequel.java
+++ part02/18-sumofasequencethesequel/SumOfASequenceTheSequel.java
@@ -6,5 +6,26 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("First number?");
+
+        int firstNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Last number?");
+
+        int secondNum = Integer.valueOf(scanner.nextLine());
+
+        if (firstNum > secondNum) {
+            System.out.println("The first number must be smaller than the last number.");
+            return;
+        }
+
+        int sum = 0;
+
+        for (int i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum is: " + sum);
+
     }
 }
