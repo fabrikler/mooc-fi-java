775093e5ea749fcd8b2007649986df1bd3179f63
fabrikler <hi@carlosmeyer.com>
2026-04-03 11:56:21 +0200

    more division

--- part02/25-division/Division.java
+++ part02/25-division/Division.java
@@ -3,13 +3,11 @@
 public class Division {
 
     public static void main(String[] args) {
-        
-        // Once you have implemented the division method, you can
-        // try it out here. Fopr example division(3,5);
-        // should print "0.6"
-
-        // division(3, 5);
+        division(3, 5);
     }
 
-    // implement the method here
+    public static void division(int num1, int num2) {
+        double result = (double) num1 / num2;
+        System.out.println(result);
+    }
 }
--- part02/26-divisiblebythree/DivisibleByThree.java
+++ part02/26-divisiblebythree/DivisibleByThree.java
@@ -6,6 +6,19 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        divisibleByThreeInRange(2, 10);
+
+    }
+
+    public static void divisibleByThreeInRange(int num1, int num2) {
+        for (int i = num1; i <= num2; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            } 
+        }
+
     }
 
 }
+
+
