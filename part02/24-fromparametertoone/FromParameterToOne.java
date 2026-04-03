b1dbc7f02c6d3af0a895e81171e7ef6d9a9c1500
fabrikler <hi@carlosmeyer.com>
2026-04-03 11:20:31 +0200

    method parameters

--- part02/24-fromparametertoone/FromParameterToOne.java
+++ part02/24-fromparametertoone/FromParameterToOne.java
@@ -1,10 +1,16 @@
-
-
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        printFromNumberToOne(10);
+    }
 
+    public static void printFromNumberToOne(int number) {
+        int counter = number;
 
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
     }
 
 }
