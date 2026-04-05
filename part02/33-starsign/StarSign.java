55f31fbaec524cc36f1a641f2a37132e7e63f063
fabrikler <hi@carlosmeyer.com>
2026-04-05 22:11:53 +0200

    stars and triangles

--- part02/33-starsign/StarSign.java
+++ part02/33-starsign/StarSign.java
@@ -8,29 +8,39 @@ public class StarSign {
         //NB: If the tests don't seem to pass, you should try the methods here
         //in the main to make sure they print the correct shapes!
         
-        printStars(3);
-        System.out.println("\n---");  // printing --- between the shapes
-        printSquare(4);
-        System.out.println("\n---");
-        printRectangle(5, 6);
-        System.out.println("\n---");
+        // printStars(5);
+        // printStars(3);
+        // printStars(9);
+        // System.out.println("\n---");  // printing --- between the shapes
+        //printSquare(4);
+        // System.out.println("\n---");
+        // printRectangle(5, 6);
+        // System.out.println("\n---");
         printTriangle(3);
-        System.out.println("\n---");
+        // System.out.println("\n---");
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        } System.out.println();
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(size);
+        } 
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        for (int i = 1; i <= height; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+        }
     }
 }
