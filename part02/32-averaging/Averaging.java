e2c370da664ef60fe4941f9e705cb58aecaca359
fabrikler <hi@carlosmeyer.com>
2026-04-05 10:15:08 +0200

    basic methods with return

--- part02/32-averaging/Averaging.java
+++ part02/32-averaging/Averaging.java
@@ -3,13 +3,14 @@ public class Averaging {
 
     // implement the sum method here again
     public static int sum(int number1, int number2, int number3, int number4) {
-        // write some code here
-        return 0;
+        int result = number1 + number2 + number3 + number4;
+        return result;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
-        //write some code here
-        return -1;
+        int sum = sum(number1, number2, number3, number4);
+        double result = sum / 4.0;
+        return result;
     }
 
     public static void main(String[] args) {
