c601b55292cad2d6b6aa98962c07a55b77c4f941
fabrikler <hi@carlosmeyer.com>
2026-04-17 21:55:54 +0200

    oldest string to int

--- part03/30-ageoftheoldest/AgeOfTheOldest.java
+++ part03/30-ageoftheoldest/AgeOfTheOldest.java
@@ -6,6 +6,28 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldest = 0;
+        int age = 0;
 
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splitted = input.split(",");
+
+            age = Integer.valueOf(splitted[1]);
+
+            if (oldest < age) {
+                oldest = age;
+            }
+
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
     }
 }
+
