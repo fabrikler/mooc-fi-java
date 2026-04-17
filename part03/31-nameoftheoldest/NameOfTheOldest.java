c6842623a7795f2669f5cabb504a1da4a4c7ae91
fabrikler <hi@carlosmeyer.com>
2026-04-17 22:14:03 +0200

    oldest name

--- part03/31-nameoftheoldest/NameOfTheOldest.java
+++ part03/31-nameoftheoldest/NameOfTheOldest.java
@@ -6,6 +6,30 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldestAge = 0;
+        int tempAge = 0;
+        String oldestName = "";
+        String tempName = "";
+
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] split = input.split(",");
+            tempName = split[0];
+            tempAge = Integer.valueOf(split[1]);
+
+            if (oldestAge < tempAge) {
+                oldestAge = tempAge;
+                oldestName = tempName;
+            }
+        }
+
+        System.out.println("Name of the oldest: " + oldestName);
 
     }
 }
