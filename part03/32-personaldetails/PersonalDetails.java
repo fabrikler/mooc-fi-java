269d402580219546e7f1c6a2a005391145467322
fabrikler <hi@carlosmeyer.com>
2026-04-17 22:37:03 +0200

    personal details

--- part03/32-personaldetails/PersonalDetails.java
+++ part03/32-personaldetails/PersonalDetails.java
@@ -1,5 +1,3 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PersonalDetails {
@@ -7,6 +5,34 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        String longestName = "";
+        int counter = 0;
+
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splitted = input.split(",");
+            sum += Double.valueOf(splitted[1]);
+            counter++;
+
+            if (longestName.length() < splitted[0].length()) {
+                longestName = splitted[0];
+            }
+        }
+
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + sum / counter);
+
+        scanner.close();
 
     }
+
 }
+
+
