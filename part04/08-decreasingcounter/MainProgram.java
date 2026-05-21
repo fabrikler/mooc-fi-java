6ad8895019ebc89fdba93e2fee113f67516614f1
fabrikler <hi@carlosmeyer.com>
2026-05-21 08:50:23 +0200

    decreasing counter

--- part04/08-decreasingcounter/MainProgram.java
+++ part04/08-decreasingcounter/MainProgram.java
@@ -5,7 +5,16 @@ public class MainProgram {
         DecreasingCounter counter = new DecreasingCounter(10);
 
         counter.printValue();
+
         counter.decrement();
+        counter.printValue();
+
         counter.decrement();
+        counter.printValue();
+
+        counter.reset();
+
+        counter.printValue();
+
     }
 }
